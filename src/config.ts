2cf7d83138b3cc6b9851cc7f50c6e6c1f02dcaba
Ákos Imre <19newvillage92@gmail.com>
2021-11-13 18:53:11 +0100

    feat: 🎸 Add dotenv with example

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 export const PROD = process.env.NODE_ENV === 'production';
 export const DEV = process.env.NODE_ENV === 'development';
 export const TEST = process.env.NODE_ENV === 'test';
